f2d2ae5abaf224a96bcd5c3de57b96fd8db5aca5
Victor Viterbo <vic.viterbo@gmail.com>
2025-08-31 20:19:25 +0200

    ALL GOOOOOOOOOOD

--- CPP09/ex02_new/srcs/FordJohnson.cpp
+++ CPP09/ex02_new/srcs/FordJohnson.cpp
@@ -6,7 +6,7 @@
 /*   By: victorviterbo <victorviterbo@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 13:02:46 by vviterbo          #+#    #+#             */
-/*   Updated: 2025/08/31 17:16:37 by victorviter      ###   ########.fr       */
+/*   Updated: 2025/08/31 20:19:02 by victorviter      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,10 @@ std::deque<int>	recursiveMergeInsert(std::deque<int> mixed)
 
 	std::cout << "Mixed step 0" << std::endl;
 	printDeque(mixed);
-	while (i + 1 < mixed.size())
+	while (i < mixed.size())
 	{
+		if (i + 1 == mixed.size())
+			predecessors[-1] = mixed[i];
 		else if (mixed[i] > mixed[i + 1])
 		{
 			highVals.push_back(mixed[i]);
@@ -44,11 +46,13 @@ std::deque<int>	recursiveMergeInsert(std::deque<int> mixed)
 	
 	if (highVals.size() > 2)
 		main = recursiveMergeInsert(highVals);
-	else if (!main.size())
+	else if (highVals.size() > 1)
 	{
 		main.push_back(highVals[0] < highVals[1] ? highVals[0] : highVals[1]);
 		main.push_back(highVals[0] < highVals[1] ? highVals[1] : highVals[0]);
 	}
+	else
+		main.push_back(highVals[0]);
 	std::cout << "main step 2" << std::endl;
 	printDeque(main);
 	std::cout << "highVals step 2" << std::endl;
@@ -58,7 +62,7 @@ std::deque<int>	recursiveMergeInsert(std::deque<int> mixed)
 
 
 	
-	mergeInMain(main, highVals, predecessors);
+	mergeInMain(main, predecessors);
 
 	
 	std::cout << "main step 3" << std::endl;
@@ -66,18 +70,29 @@ std::deque<int>	recursiveMergeInsert(std::deque<int> mixed)
 	return (main);
 }
 
-void	mergeInMain(std::deque<int> &main, std::deque<int> highVals, std::map<int, int> predecessors)
+void	mergeInMain(std::deque<int> &main, std::map<int, int> predecessors)
 {
-	main.push_front(predecessors[highVals[0]]);
+	std::deque<int>	reorderedLowVals;
+
+	printDeque(main);
+	for (std::deque<int>::iterator it = main.begin(); it != main.end(); ++it)
+	{
+		std::cout << "I see " << *it << " and I append its predecessor " << predecessors[*it] << std::endl;
+		reorderedLowVals.push_back(predecessors[*it]);
+	}
+	if (predecessors.find(-1) != predecessors.end())
+		reorderedLowVals.push_back(predecessors[-1]);
+	std::cout << "Reordered predecessors" << std::endl;
+	printDeque(reorderedLowVals);
+	main.push_front(predecessors[main[0]]);
 	std::cout << "main step inserted elem 0 of secondary chain" << std::endl;
 	printDeque(main);
-	for (size_t i = 1; i < 2 * highVals.size(); ++i)
+	for (size_t i = 0; i < std::min(2 * reorderedLowVals.size(), insertionOrder.size()); ++i)
 	{
-		if (insertionOrder[i] > highVals.size())
+		if (insertionOrder[i] > reorderedLowVals.size())
 			continue ;
-		std::cout << "Working on i " << i << " at position " << insertionOrder[i] << std::endl;
-		std::cout << "Going to insert predecessor of " << main[insertionOrder[i]] << " i.e " << predecessors[main[insertionOrder[i]]] << std::endl;
-		insertIntoMain(main, predecessors[main[insertionOrder[i]]], insertionOrder[i] + i);
+		std::cout << "Going to insert\ninsertIndex = " << insertionOrder[i] << "\ncorresponding main = " << main[insertionOrder[i]] << "\npredecessor = " << reorderedLowVals[insertionOrder[i] - 1] << std::endl;
+		insertIntoMain(main, reorderedLowVals[insertionOrder[i] - 1], insertionOrder[i] + i);
 	}
 }
 
@@ -86,7 +101,7 @@ void	insertIntoMain(std::deque<int> &main, int n, int indx)
 	int							range;
 	std::deque<int>::iterator	it = main.begin();
 
-	range = (indx + 1) / 2;
+	range = std::min((indx + 1) / 2, static_cast<int>(main.size() - 1));
 	indx = indx / 2;
 	std::cout << "Inserting element " << n << " starting comparison at pos " << indx << std::endl;
 	std::cout << "First comparison will be with " << main[indx] << std::endl;
--- CPP09/ex02_new/srcs/main.cpp
+++ CPP09/ex02_new/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: victorviterbo <victorviterbo@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 12:26:21 by vviterbo          #+#    #+#             */
-/*   Updated: 2025/08/31 16:59:27 by victorviter      ###   ########.fr       */
+/*   Updated: 2025/08/31 19:43:36 by victorviter      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,7 @@ int main(int argc, char *argv[])
 	printDeque(insertionOrder);
 	getInsertionOrder(vals.size());
 	vals = recursiveMergeInsert(vals);
-	while (vals.size())
-	{
-		std::cout << vals.back() << std::endl;
-		vals.pop_back();
-	}
+	for (unsigned int i = 0; i < vals.size() - 1; ++i)
+		std::cout << vals[i] << ", ";
+	std::cout << vals.back() << std::endl;
 }
