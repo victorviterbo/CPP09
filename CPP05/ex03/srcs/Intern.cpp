4a4c75a80e06629d814cd27d59548418dc74ed3b
victorviterbo <vic.viterbo@gmail.com>
2025-10-14 14:49:32 +0200

    tmp

--- CPP05/ex03/srcs/Intern.cpp
+++ CPP05/ex03/srcs/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: victorviterbo <victorviterbo@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 22:54:56 by victorviter       #+#    #+#             */
-/*   Updated: 2025/10/14 14:43:07 by victorviter      ###   ########.fr       */
+/*   Updated: 2025/10/14 14:44:48 by victorviter      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,39 +24,9 @@ Intern &Intern::operator=(Intern &other) {
 }
 
 Intern::~Intern() {}
-/*
-static AForm	*makePresidentialPardonForm(std::string formTarget)
-{
-	return (new PresidentialPardonForm(formTarget));
-}
 
-static AForm	*makeRobotomyRequestForm(std::string formTarget)
-{
-	return (new RobotomyRequestForm(formTarget));
-}
-
-static AForm	*makeShrubberyCreationForm(std::string formTarget)
-{
-	return (new ShrubberyCreationForm(formTarget));
-}
-*/
 AForm	*Intern::makeForm(std::string formName, std::string formTarget)
 {
-	/*const int       FormStackSize = 3;
-	std::string 	names[FormStackSize] = {"PresidentialPardonForm", "RobotomyRequestForm", "ShrubberyCreationForm"};
-	AForm            *(*formStack[FormStackSize])(std::string formTarget) = \
-						{&makePresidentialPardonForm, \
-						 &makeRobotomyRequestForm, \
-						 &makeShrubberyCreationForm};
-	
-	for (unsigned int i = 0; i < FormStackSize; i++)
-	{
-		if (!names[i].compare(formName))
-		{
-			std::cout << "Intern creates " << formName << std::endl;
-			return (formStack[i](formTarget));
-		}
-	}*/
 	if (formName == "PresidentialPardonForm")
 		return (new PresidentialPardonForm(formTarget));
 	else if (formName == "RobotomyRequestForm")
