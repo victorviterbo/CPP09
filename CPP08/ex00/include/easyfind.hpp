001f2f9b32ed816faf397136627ebfd560ece35f
Victor Luc Etienne Viterbo <vviterbo@c3r3s5.42lausanne.ch>
2025-10-30 11:54:36 +0100

    improved ex00

--- CPP08/ex00/include/easyfind.hpp
+++ CPP08/ex00/include/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 14:00:00 by victorviter       #+#    #+#             */
-/*   Updated: 2025/10/28 15:40:32 by vviterbo         ###   ########.fr       */
+/*   Updated: 2025/10/30 11:53:27 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,15 @@
 #include <algorithm>
 
 template < typename T >
-int easyfind(T container, int value)
+typename T::iterator easyfind(T &container, int value)
 {
 	typename T::iterator	it = container.begin();
 
 	while (it != container.end())
 	{
 		if (*it == value)
-			return (*it);
+			return (it);
 		++it;
 	}
-	throw std::runtime_error("Value not in container");
+	return (it);
 }
--- CPP08/ex00/srcs/main.cpp
+++ CPP08/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 14:14:33 by victorviter       #+#    #+#             */
-/*   Updated: 2025/10/28 15:43:19 by vviterbo         ###   ########.fr       */
+/*   Updated: 2025/10/30 11:51:57 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,33 +19,21 @@ int main(void)
 	std::vector<int>	v(arr1, arr1 + 5);
 	std::list<int>		l(arr2, arr2 + 5);
 
-	try {
-		easyfind(v, 3);
-		std::cout << "Value found in container" << std::endl;
-	}
-	catch (std::runtime_error &e){
-		std::cout << e.what() << std::endl;
-	}
-	try {
-		easyfind(v, 4);
-		std::cout << "Value found in container" << std::endl;
-	}
-	catch (std::runtime_error &e){
-		std::cout << e.what() << std::endl;
-	}
-	try {
-		easyfind(l, 'a');
-		std::cout << "Value found in container" << std::endl;
-	}
-	catch (std::runtime_error &e){
-		std::cout << e.what() << std::endl;
-	}
-	try {
-		easyfind(l, 'z');
-		std::cout << "Value found in container" << std::endl;
-	}
-	catch (std::runtime_error &e){
-		std::cout << e.what() << std::endl;
-	}
+		if (easyfind(v, 3) != v.end())
+			std::cout << "Value found in container" << std::endl;
+		else
+			std::cout << "Value not in container" << std::endl;
+		if (easyfind(v, 4) != v.end())
+			std::cout << "Value found in container" << std::endl;
+		else
+			std::cout << "Value not in container" << std::endl;
+		if (easyfind(l, 'a') != l.end())
+			std::cout << "Value found in container" << std::endl;
+		else
+			std::cout << "Value not in container" << std::endl;
+		if (easyfind(l, 'z') != l.end())
+			std::cout << "Value found in container" << std::endl;
+		else
+			std::cout << "Value not in container" << std::endl;
 	return (0);
 }
